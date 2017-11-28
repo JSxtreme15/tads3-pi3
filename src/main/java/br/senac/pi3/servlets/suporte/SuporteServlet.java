d89730fb8f853ab8aa8c684f6a21561427a2bde8
orgm <orgm@DESKTOP-37VO208>
2017-11-28 15:11:15 -0200

    Tentativa de envio de email

--- src/main/java/br/senac/pi3/servlets/suporte/SuporteServlet.java
+++ src/main/java/br/senac/pi3/servlets/suporte/SuporteServlet.java
@@ -5,14 +5,21 @@
  */
 package br.senac.pi3.servlets.suporte;
 
-import java.io.*;
-import java.util.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.mail.*;
-import javax.mail.Session;
-import javax.mail.internet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.Session;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+
 
 /**
  *
@@ -21,58 +28,28 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(name = "SuporteServlet", urlPatterns = {"/suporte"})
 public class SuporteServlet extends HttpServlet {
 
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html;charset=UTF-8");
-        String Assunto = request.getParameter("Assunto");
-        String Mensagem = request.getParameter("Mensagem");
-
-        // Recipient's email ID needs to be mentioned.
-        String to = "tardis.suporte@gmail.com";
-
-        // Sender's email ID needs to be mentioned
-        String from = "tardis.suporte@gmail.com";
-
-      // Assuming you are sending email from localhost
-      String host = "localhost";
- 
-      // Get system properties
-      Properties properties = System.getProperties();
- 
-      // Setup mail server
-      properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-        // Set response content type
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
         try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject(request.getParameter("Assunto"));
-
-            // Now set the actual message
-            message.setText(request.getParameter("Mensagem"));
-
-            // Send message
-            Transport.send(message);
-            
-        }  catch (javax.mail.MessagingException ex) {
-            System.out.println(ex.getStackTrace());
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html;charset=UTF-8");
+            String Assunto = request.getParameter("Assunto");
+            String Mensagem = request.getParameter("Mensagem");
+            enviaEmail();
+        } catch (EmailException ex) {
+            Logger.getLogger(SuporteServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-            response.sendRedirect(request.getContextPath() + "/telaInicial?enviado=true");
-        }
+        
     }
+    
+    public void enviaEmail() throws EmailException{
+        SimpleEmail email = new SimpleEmail();
+        email.setHostName("smtp.gmail.com");
+        email.setSmtpPort(465);
+        email.setFrom("tardis.suporte@gmail.com");
+        email.addTo("tardis.suporte@gmail.com");
+        email.setSubject("teste");
+        email.setMsg("teste");
+        email.setSSL(true);
+        email.setAuthentication("tardis.suporte@gmail.com", "tardis123");
+        email.send();
+}}
